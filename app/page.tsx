769042abe0f67b0b6c6723448ac6c8f66caa220e
rhodyapps <12385176+rhodyapps@users.noreply.github.com>
2024-03-20 06:01:44 -0400

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className='chatbot-main'>
+      <div className='container'>
+        <h1 className='text-3xl font-bold'>ChatBot App</h1>
+      </div>
+    </section>
+  )
+}
